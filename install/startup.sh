049f958dcaab938643d747ab784a9ffb72c0bad7
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-11 21:26:07 -0700

    fix command line tools installation

--- install/startup.sh
+++ install/startup.sh
@@ -1,6 +1,14 @@
-installBilow $((if [[ "$machine" = "MacOSX" ]]; then "mac"; else "ubuntu"; fi))
+
+if [[ $machine = "MacOSX" ]]
+then 
+    installBilow mac
+else
+    installBilow ubuntu
+fi
+
 installBilow anaconda
 installBilow work_env
 installBilow tensorflow_env
+
 installBilow completion
 installBilow bashrc
--- install/tensorflow_env.sh
+++ install/tensorflow_env.sh
@@ -1,9 +1,9 @@
 # Install tensorflow python
 PYTHON_VERSION=3.6
 rm -rf $conda_dir/envs/tf
-conda create -n tf -y python=$PYTHON_VERSION
+$conda_dir/bin/conda create -n tf -y python=$PYTHON_VERSION
 
-conda install -n tf -y \
+$conda_dir/bin/conda install -n tf -y \
   tensorflow-gpu \
   numpy \
   pandas \
