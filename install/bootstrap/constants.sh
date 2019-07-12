893d8351ecd898a88913864c828904103543e080
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-11 22:21:21 -0700

    add constants to functions

--- install/bootstrap/constants.sh
+++ install/bootstrap/constants.sh
@@ -1,3 +1,13 @@
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=MacOSX;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+export machine=$machine
+export conda_dir=$HOME/miniconda
+
 export linuxAndMac=<<EOF
     gcc \
     vim \
--- install/bootstrap/functions.sh
+++ install/bootstrap/functions.sh
@@ -1,16 +1,5 @@
 bilowGithub="https://raw.githubusercontent.com/michaelbilow/dotfiles/master/install"
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     machine=Linux;;
-    Darwin*)    machine=MacOSX;;
-    *)          machine="UNKNOWN:${unameOut}"
-esac
-
-export machine=$machine
-export conda_dir=$HOME/miniconda
-
-
 getBilow() {
     filename="$1.sh"
     curl -fsSL "${bilowGithub}/${filename}" -o $filename
@@ -24,9 +13,16 @@ catBilow() {
 
 installBilow() {
     filename=$(getBilow $1)
-    sed -i "1s;^;source /tmp/functions.sh\n;" $filename
+    sed -i "1s;^;source /tmp/functions.sh\nsource /tmp/constants.sh\n;" $filename
     shift
     chmod +x $filename
     sudo bash ${filename} $@
     rm $filename
 }
+
+if [[ ! -f constants.sh ]]
+then
+    mv $(getBilow "bootstrap/constants.sh") /tmp/
+fi
+
+source /tmp/constants.sh
