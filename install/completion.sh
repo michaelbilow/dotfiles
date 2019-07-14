45e4481ec0556539e72e2d40267302962c2de138
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-14 16:35:47 -0700

    typos in completion

--- install/completion.sh
+++ install/completion.sh
@@ -1,2 +1,2 @@
-curl -fsSL https://github.com/git/git/blob/master/contrib/completion/git-completion.bash -o $HOME/.git-completion.bash
-curl -fsSL https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh -o $HOME/.git-prompt.sh
+curl -fsSL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -o $HOME/.git-completion.bash
+curl -fsSL https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh -o $HOME/.git-prompt.sh
