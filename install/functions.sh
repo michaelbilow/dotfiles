e6fd00ad14e7b9447f725dd53b85fba5bb4bd01a
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-14 16:00:05 -0700

    add logging, refactor some functions

--- install/functions.sh
+++ install/functions.sh
@@ -1,30 +1,57 @@
+# Install Utilities
 bilowGithub="https://raw.githubusercontent.com/michaelbilow/dotfiles/master/install"
 
 getBilow() {
     filename="$1.sh"
     curl -fsSL "${bilowGithub}/${filename}" > $filename
+    returncode=$?
+    if [[ $returncode -ne 0 ]]
+    then
+        rm $filename
+        log "$filename does not exist!"
+        exit $returncode
+    fi
     echo $filename
 }
 
 catBilow() {
-    filename="$1.sh"
-    curl "${bilowGithub}/${filename}"
+    filename=$(getBilow $1)
+    cat $filename
+    rm $filename
 }
 
 installBilow() {
     filename=$(getBilow $1)
-    sed -i "1s;^;source /tmp/functions.sh\nsource /tmp/constants.sh\ninvertText \"\\n\\nStarting \$0 from $BASH_SOURCE\\n\\n\"\n;" $filename
-    sed -e "\$a\ninvertText \"\\n\\nFinished \$0, returning to $BASH_SOURCE\\n\\n\"\n" $filename
+    addHeaderAndFooter $filename
     shift
     sudo bash ${filename} $@
     rm $filename
 }
 
+# Helper Utilities
+
 invertText() {
     echo -e "\e[7m$1\e[27m"
+}
+
+log () {
+    echo $1 >> $installLog
+}
+
+invertTextAndLog () {
+    invertText $1
+    log $1
 }
 
 askYesNo() {
     read -p "$1 (y/N) "
     echo $REPLY | tr '[:upper:]' '[:lower:]' | head -c 1
+}
+
+addHeaderAndFooter() {
+    filename=$1
+    HEADER="source /tmp/constants.sh\nsource /tmp/functions.sh\ninvertText \"\\n\\nStarting \$0 from $BASH_SOURCE\\n\\n\"\n;"
+    FOOTER="\ninvertText \"\\n\\nFinished \$0, returning to $BASH_SOURCE\\n\\n\"\n"
+    sed -i "1s;^;$HEADER" $filename
+    sed -e "\$a$FOOTER" $filename
 }
