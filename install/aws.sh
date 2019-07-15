e09492f4d22985b33e4cea2708d91e11cc74e284
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-14 19:18:02 -0700

    save aws credentials in right place

--- install/aws.sh
+++ install/aws.sh
@@ -10,6 +10,8 @@ awsInvite="Do you want to send AWS credentials?"
 if [[ $(askYesNo "$awsInvite") == "y" ]]
 then
     mkdir -p $HOME/.aws
+    cd $HOME/.aws
     echo "wormhole send your credentials from your other machine"
     wormhole receive
+    cd -
 fi
