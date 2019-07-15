3c42109b6069cf7a675210baec5aa45ce3f11849
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-14 19:12:38 -0700

    chmod .ssh dir

--- install/passwordless_ssh.sh
+++ install/passwordless_ssh.sh
@@ -1,5 +1,7 @@
+chmod 755 .ssh
 passwordlessSSH="Do you want to set up passwordless ssh from another machine?" 
 while [[ $(askYesNo "$passwordlessSSH") == "y" ]]
 do
     wormhole ssh invite
 done
+chmod 640 .ssh
--- install/ssh.sh
+++ install/ssh.sh
@@ -1,8 +1,9 @@
-mkdir -p ~/.ssh
+mkdir -p $HOME/.ssh
+chmod 755 $HOME/.ssh
 read -p "Email for ssh key: " email
-ssh-keygen -t rsa -b 4096 -C $email -f ~/.ssh/id_rsa
+ssh-keygen -t rsa -b 4096 -C $email -f $HOME/.ssh/id_rsa
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+ssh-add $HOME/.ssh/id_rsa
 
 githubSSH="Do you want to set up a github token for this ssh key?"
 if [[ $(askYesNo "$githubSSH") == "y" ]]
@@ -11,9 +12,11 @@ then
     read -p "Name your new github ssh key: " pwName
 
     ghData=$( jq -n \
-            --arg key "$(cat ~/.ssh/id_rsa.pub)" \
+            --arg key "$(cat $HOME/.ssh/id_rsa.pub)" \
             --arg title "$pwname" \
             '{title: $title, key: $key}' )
 
     curl -u "$email:$ghPass" --data $ghData https://api.github.com/user/keys
 fi
+
+chmod 640 $HOME/.ssh
