7ee1b998f1158f842837d9fc7b5ba26484ce816b
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-17 14:24:52 -0700

    fix github

--- github/01_github.sh
+++ github/01_github.sh
@@ -1,23 +1,20 @@
-githubSSH="Do you want to set up a github token for your ssh key at $HOME/.ssh/id_rsa.pub?"
-if [[ $(askYesNo "$githubSSH") == "y" ]]
-then
-    echo "sorry, not working with 2FA right now"
-    exit 0
-    # TODO: Fix!
-    read -sp "Github password: " ghPass
-    echo ""
-    read -p "Name your new github ssh key: " pwName
 
-    ghData=$(jq -n \
-                --arg key "$(cat $HOME/.ssh/id_rsa.pub)" \
-                --arg title "$pwname" \
-                '{title: $title, key: $key}' )
+echo "sorry, not working with 2FA right now"
+exit 0
+# TODO: Fix!
+read -sp "Github password: " ghPass
+echo ""
+read -p "Name your new github ssh key: " pwName
 
-    curl --request POST \
-        --url https://api.github.com/authorizations \
-        --header 'authorization: Basic PASSWORD' \
-        --header 'content-type: application/json' \
-        --header "x-github-otp: $OTP" \
-        --data '{"scopes": ["public_repo"], "note": "test"}'
-    curl -u "$email:$ghPass" --data $ghData https://api.github.com/user/keys
-fi
+ghData=$(jq -n \
+            --arg key "$(cat $HOME/.ssh/id_rsa.pub)" \
+            --arg title "$pwname" \
+            '{title: $title, key: $key}' )
+
+curl --request POST \
+    --url https://api.github.com/authorizations \
+    --header 'authorization: Basic PASSWORD' \
+    --header 'content-type: application/json' \
+    --header "x-github-otp: $OTP" \
+    --data '{"scopes": ["public_repo"], "note": "test"}'
+curl -u "$email:$ghPass" --data $ghData https://api.github.com/user/keys
