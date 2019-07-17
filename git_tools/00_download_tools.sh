0ba5dc5ad0424441779f56c320e3455d415b9b73
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-17 14:23:44 -0700

    move files

--- git_tools/00_download_tools.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p $HOME/bin
-echo "export PATH=\"\$PATH:$HOME/bin\"" >> ~/.bashrc
-
-git_dl () {
-    outputPath=$HOME/bin/$2
-    curl -sL $1 > $outputPath && chmod +x $outputPath
-
-}
-git_dl https://github.com/spencertipping/ni/raw/develop/ni ni
