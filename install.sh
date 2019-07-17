f00bd58ab5f587bce9744f7d60f6e54ac3807d55
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-16 23:43:20 -0700

    replace BASH_SOURCE/.. with dirname

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-source "$(dirname $BASH_SOURCE)/../helpers/constants.sh"
-source "$(dirname $BASH_SOURCE)/../helpers/functions.sh"
+source "$(dirname $BASH_SOURCE)/helpers/constants.sh"
+source "$(dirname $BASH_SOURCE)/helpers/functions.sh"
 installLogfile="/tmp/$(date +%Y%m%d%H%M%S)_install.log"
 
 if [[ $machine != "mac" && $machine != ubuntu ]]
