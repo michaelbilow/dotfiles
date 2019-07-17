1459a77e856a300a5c09205f73b5b8d426a3273d
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-17 11:56:46 -0700

    typo in nvidia, do not re-download ripgrep

--- ubuntu/02_system.sh
+++ ubuntu/02_system.sh
@@ -4,9 +4,12 @@ sudo apt-get install -y \
     $linuxAndMac \
     $linuxOnly
 
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb
-sudo dpkg -i ripgrep_11.0.1_amd64.deb
-rm ripgrep_11.0.1_amd64.deb
+if [[ ! $(command -v rg) ]]
+then
+    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.1/ripgrep_11.0.1_amd64.deb
+    sudo dpkg -i ripgrep_11.0.1_amd64.deb
+    rm ripgrep_11.0.1_amd64.deb
+fi
 
 sudo apt-get autoremove
 sudo apt-get autoclean
--- ubuntu/04_nvidia.sh
+++ ubuntu/04_nvidia.sh
@@ -3,6 +3,6 @@ then
     sudo apt-get -o Dpkg::Options::="--force-overwrite" install -y cuda-10-0 cuda-drivers
     echo 'export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}' >> $homeDir/.bashrc
     echo 'export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> $homeDir/.bashrc
-    source ~/.bashrc
+    source $homeDir/.bashrc
     sudo ldconfig
 fi
