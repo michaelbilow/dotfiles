7eb2ebd3b92a7c2cd9bbc13643930268333e7a70
Michael Bilow <michael.k.bilow@gmail.com>
2019-07-18 15:42:08 -0700

    more bashrc fixes

--- bash/02_bashrc.sh
+++ bash/02_bashrc.sh
@@ -1,10 +1,14 @@
+cat <<EOF >> $homeDir/.bashrc
+source $homeDir/.git-prompt.sh
+source $homeDir/.git-completion.bash
+export PATH="/usr/local/bin:/usr/local/sbin:$homeDir/bin:\$PATH"
+EOF
+
 staticScriptsDir="$(dirname $(dirname $BASH_SOURCE))/static_scripts"
 cat "${staticScriptsDir}/bash_prompt_helpers.sh" >> $homeDir/.bashrc
 cat "${staticScriptsDir}/bash_variables.sh" >> $homeDir/.bashrc
 cat "${staticScriptsDir}/bash_aliases.sh" >> $homeDir/.bash_aliases
 cat "${staticScriptsDir}/bash_profile.sh" >> $homeDir/.bash_profile
-printf "export PATH=\"/usr/local/bin:/usr/local/sbin:$homeDir/bin:\$PATH\"\n" >> $homeDir/.bashrc
-
 
 if [[ $machine = "mac" ]]
 then
--- static_scripts/bash_variables.sh
+++ static_scripts/bash_variables.sh
@@ -1,3 +1,2 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-export PATH="/usr/local/bin:/usr/local/sbin:$homeDir/bin:$PATH"
